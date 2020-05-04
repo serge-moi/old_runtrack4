f56d3beff9769e480ebabdf470a9d1be2b4246f4
serge <serge.moi@laplateforme.io>
2020-05-04 11:22:09 +0200

    La maquette manque un peu de réalisme, c'est pas possible de la reproduire a la perfection, c'est dommage ...

---
+++ jour01/job03/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>job03</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+    <main>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+    </main>
+</body>
+</html>
