81ab9e48acb9e4d57ae4bf4378a8a05a5c7c1433
serge <serge.moi@laplateforme.io>
2020-05-04 13:43:23 +0200

    Je crois que c'est bon, sans conviction, je sais pas vraiment si c'est le résultat attendu, mais bon ça y ressemble. target c'est bof quand on a un outil aussi utile que js, mais c'était peut être le but de l'exo, plouf.

---
+++ jour01/job04/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>job04</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+    <header id="main-headbox">
+        <a href="#nav-headbox" id="but-burg"></a>
+        <nav id="nav-headbox">
+            <ul id="nav-ul">
+                <li><a href="#" class="lien-headbox">Accueil</a></li>
+                <li><a href="#" class="lien-headbox">Présentation</a></li>
+                <li><a href="#" class="lien-headbox">Contact</a></li>
+            </ul>
+        </nav>  
+    </header>
+    <main>
+        ---
+    </main>
+    <footer>
+        ---
+    </footer>
+</body>
+</html>
