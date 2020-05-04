de707c69337b4a0c01cfb93251a4f24bf0ab7a3d
serge <serge.moi@laplateforme.io>
2020-05-04 09:59:29 +0200

    Les bases des medias queries on crée des pages style.css selon la taille de la fenêtre

---
+++ jour01/job01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style1.css" type="text/css">
+    <link rel="stylesheet" href="css/style2.css" type="text/css">
+    <link rel="stylesheet" href="css/style3.css" type="text/css">
+    <link rel="stylesheet" href="css/style4.css" type="text/css">
+    <title>job01</title>
+</head>
+<body>
+</body>
+</html>
