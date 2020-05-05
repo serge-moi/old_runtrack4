d9ea22c775f6d4fbaa228d4f02e7e495687ed616
serge <serge.moi@laplateforme.io>
2020-05-06 00:14:14 +0200

    ce sont des photos légal hein, c'est le seul moyen que j'ai trouvé pour m'amuser :/

---
+++ jour02/job05/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf8">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+
+    <body class="blue-grey lighten-5">
+        <header >
+            <nav class="teal accent-2">
+                <div class="container nav-wrapper">
+                    <a href="#" class="brand-logo  black-text"><i class="small material-icons">albumalbum</i>Co-nudes</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html" class="black-text">Sass</a></li>
+                        <li><a href="badges.html" class="black-text">Components</a></li>
+                        <li><a href="collapsible.html" class="black-text">JavaScript</a></li>
+                        <li><a href="collapsible.html" class="black-text">JavaScript</a></li>
+                        <li><a href="collapsible.html"  class="black-text">JavaScript</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="container row">
+                <h5 class="grey-text small">sexy celebrity</h5>
+                <div class="card col l4 offset-l2">
+                    <div class="card-image waves-effect waves-block waves-light row">
+                        <img class="activator " src="img/emily.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">Emily Ratajkowski</span>
+                        <div>
+                            <a href="#">voir plus</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card col l2 offset-l2">
+                    <div class="card-image waves-effect waves-block waves-light row">
+                        <img class="activator" src="img/gal.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">Gal Gadot</span>
+                        <div>
+                            <a href="#">voir plus</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class=" s1 col l4 offset-l1">
+                            <div class="card horizontal">
+                                <div class="card-image">
+                                    <img src="img/amber.jpg">
+                                </div>
+                                <div class="card-content">
+                                    <span class="card-title activator grey-text text-darken-4 ">Amber Heard</span>
+                                    <p>
+                                        <a href="#">voir plus</a>
+                                    </p>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container row">
+                <h6 class="grey-text small">Encore plus de photos sexys...</h6>
+                <div class="col l3 offset-l2 s3">
+                    <div class="card large">
+                        <div class="card-image">
+                            <img src="img/olivia.jpg">
+                        </div>
+                        <div class="card-stacked">
+                            <div class="card-content">
+                                <p>Olivia Wild</p>
+                            </div>
+                            <div class="card-action">
+                                <a href="#">voir plus...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col l3 s3">
+                    <div class="card large">
+                        <div class="card-image">
+                            <img src="img/gossip.jpg">
+                        </div>
+                        <div class="card-stacked">
+                            <div class="card-content">
+                                <p>Leighton Meester</p>
+                            </div>
+                            <div class="card-action">
+                                <a href="#">voir plus...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col l3 s3">
+                    <div class="card large">
+                        <div class="card-image">
+                            <img src="img/sasha.jpg">
+                        </div>
+                        <div class="card-stacked">
+                            <div class="card-content">
+                                <p>Sasha Grey</p>
+                            </div>
+                            <div class="card-action">
+                                <a href="#">voir plus...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+    
+</html>
