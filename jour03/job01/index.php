00838c7f058d86ded5afc91fc89cd632038fbb0b
serge <serge.moi@laplateforme.io>
2020-05-06 15:18:16 +0200

    ...

---
+++ jour03/job01/index.php
@@ -0,0 +1,172 @@
+<!doctype html>
+<html lang="en">
+    <head>
+
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+        <title>Bootstrap</title>
+
+    </head>
+    <body class="alert-secondary">
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">LPTF</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="#">Accueil <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link" href="#">Units</a>
+                        <a class="nav-item nav-link" href="#">Jobs</a>
+                        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Skills</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main class="container-fluid">
+            <h1 class="display-4 row justify-content-center">LaPlateforme_</h1>
+            <section class="row justify-content-around">
+                <div class="card large" style="width: 28rem;">
+                    <img src="img/papillon.jpg" class="card-img-top" alt="Accroche HTML">
+                    <div class="card-body">
+                        <h3 class="card-subtitle">Un papillon</h3>
+                        <p class="card-text">Un papillon, c'est un peu comme une chenille, mais avec des ailes. Ne pas ingerer.</p>
+                        <a href="#" class="btn btn-primary">Commander votre propre papillon</a>
+                    </div>
+                </div>
+                <div class="jumbotron col-8">
+                    <h1 class="display-4">Bonjour, monde!</h1>
+                    <p class="lead">Il existe plusieurs visions du terme:
+                    </p>
+                    <p class="lead">le monde est la matière, l'espace et les phénomènes qui nous sont accessibles par les sens. l'expérience ou la raison.</p>
+                    <p class="lead">Le sens le plus courant désigne notre planète, la Terre avec ses habitants, et son environnement plus ou moins naturel.</p>
+                    <hr class="my-4">
+                    <p>Le sens étendu désigne l'univers dans son ensemble</p>
+                    <a class="btn btn-danger btn-lg" href="#" role="button">Rebooter le Monde</a>
+                    <div class="spinner-border text-info" role="status">
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                    <nav aria-label="Page navigation example row container">
+                        <ul class="pagination justify-content-end">
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">1</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">2</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">3</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="list-group col-1">
+                    <a href="#" class="list-group-item list-group-item-action active">Limbes</a>
+                    <a href="#" class="list-group-item list-group-item-action">Luxure</a>
+                    <a href="#" class="list-group-item list-group-item-action">Gourmandise</a>
+                    <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Avarice</a>
+                    <a href="#" class="list-group-item list-group-item-action">Colere</a>
+                    <a href="#" class="list-group-item list-group-item-action">Heresie</a>
+                    <a href="#" class="list-group-item list-group-item-action">Violence</a>
+                    <a href="#" class="list-group-item list-group-item-action">Ruse et Tromperie</a>
+                    <a href="#" class="list-group-item list-group-item-action">Trahison</a>
+                    <a href="#" class="list-group-item list-group-item-action">Internet Explorer</a>
+                </div>
+            </section>
+            <div class="container">
+                
+            <h5 class="row justify-content-end col-12">Installation de AI 9000</h5>
+                <div class="row justify-content-center">
+                    <svg class="bi bi-arrow-bar-left row col-1" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 00-.708 0l-3 3a.5.5 0 000 .708l3 3a.5.5 0 00.708-.708L3.207 8l2.647-2.646a.5.5 0 000-.708z" clip-rule="evenodd" class="row justify-content-end"/>
+                            <path fill-rule="evenodd" d="M10 8a.5.5 0 00-.5-.5H3a.5.5 0 000 1h6.5A.5.5 0 0010 8zm2.5 6a.5.5 0 01-.5-.5v-11a.5.5 0 011 0v11a.5.5 0 01-.5.5z" clip-rule="evenodd"/>
+                        </svg>
+                    <div class="progress container row col-10">
+                        
+                        <div class="progress-bar progress-bar-striped bg-warning row just " role="progressbar" style="width: 95%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                        
+                    </div>
+                    <svg class="bi bi-arrow-bar-right col-1 row" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M10.146 4.646a.5.5 0 01.708 0l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L12.793 8l-2.647-2.646a.5.5 0 010-.708z" clip-rule="evenodd"/>
+                            <path fill-rule="evenodd" d="M6 8a.5.5 0 01.5-.5H13a.5.5 0 010 1H6.5A.5.5 0 016 8zm-2.5 6a.5.5 0 01-.5-.5v-11a.5.5 0 011 0v11a.5.5 0 01-.5.5z" clip-rule="evenodd"/>
+                    </svg>
+                </div>
+            </div>
+
+            <section>
+                <form class="row justify-content-around">
+                    <div class="col-3">
+                        <label for="basic-url">Recevez votre copie gratuite d'internet 2!</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1">@</span>
+                            </div>
+                            <input type="text" class="form-control" placeholder="Login" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Mot de passe" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <span class="input-group-text" id="basic-addon2">@example.com</span>
+                            </div>
+                        </div>
+                        <label for="basic-url">URL des internets 2 et 2.1 Beta</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">DogeCoin</span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                            <div class="input-group-append">
+                                <span class="input-group-text">.00</span>
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">https://l33t.lptf/dkwb/berlusconimkt/</span>
+                            </div>
+                            <textarea class="form-control" aria-label="With textarea"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="form-group from-group-lg">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </section>
+        </main>
+    
+    </body>
+</html>
